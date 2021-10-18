0c4a4e56e288b8d56ca38828390cbdd2c2434196
weaverHope <92676579+weaverHope@users.noreply.github.com>
2021-10-18 12:30:42 +0200

    Create Iteration#3.js

---
+++ Iteration#3.js
@@ -0,0 +1,28 @@
+/*
+1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5.
+*/
+alert(10 * 5);
+
+alert(10 / 2);
+
+alert(15 % 9);
+
+let x;
+let y = 10;
+let z = 5;
+x = y + z;
+console.log(x);
+
+let x2;
+x2 = y * z;
+console.log(x2);
